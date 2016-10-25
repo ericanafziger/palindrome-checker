b3fab46c71028455a2e5971febba87969866a860
Devin Ludwig and Erica Nafziger <devin@>
2016-10-25 16:29:20 -0700

    palindrome alert works when word is and is not a palindrome; does not yet work with phrases

--- palindromes/js/scripts.js
+++ palindromes/js/scripts.js
@@ -10,10 +10,11 @@ if (array.length % 2 === 0){
 for(var index = 0; index < iterations; index += 1 ) {
   if (array[0] != array[array.length - 1]) {
     alert("This ain't a palindrome. Try again");
-    return false;
+    break;
+  } else if (index === iterations-1){
+    alert("This is a palindrome!")
   } else {
     array.splice(array.length - 1, 1);
     array.splice(0, 1);
   }
-  alert("This is a palindrome!")
 }
