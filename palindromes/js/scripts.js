b52e2d7aaf8942e4fc48f3f66c1185e24fecdddb
Devin Ludwig and Erica Nafziger <erica@>
2016-10-25 16:11:07 -0700

    create simple page that prompts for a word and tells if it is a palindrome. still in progress.

---
+++ palindromes/js/scripts.js
@@ -0,0 +1,19 @@
+var input = prompt("Enter a word:");
+var array = input.split('');
+
+if (array.length % 2 === 0){
+  var iterations = array.length / 2;
+} else if (array.length % 2 === 1) {
+  var iterations = (array.length - 1)/2;
+}
+
+for(var index = 0; index < iterations; index += 1 ) {
+  if (array[0] != array[array.length - 1]) {
+    alert("This ain't a palindrome. Try again");
+    return false;
+  } else {
+    array.splice(array.length - 1, 1);
+    array.splice(0, 1);
+  }
+  alert("This is a palindrome!")
+}
